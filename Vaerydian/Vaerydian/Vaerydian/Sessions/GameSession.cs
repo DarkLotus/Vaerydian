958736085a1c3bd8f187eba740131b6fe03ee8d0
Thomas Jonell <tjonell@woh.rr.com>
2012-05-04 17:22:21 -0400

    removing unneeded classes

--- Vaerydian/Vaerydian/Vaerydian/Sessions/GameSession.cs
+++ Vaerydian/Vaerydian/Vaerydian/Sessions/GameSession.cs
@@ -12,20 +12,6 @@ namespace Vaerydian.Sessions
     static class GameSession
     {
 
-        /// <summary>
-        /// player's character
-        /// </summary>
-        private static Character gs_PlayerCharacter;
-
-        /// <summary>
-        /// player's character
-        /// </summary>
-        public static Character PlayerCharacter
-        {
-            get { return gs_PlayerCharacter; }
-            set { gs_PlayerCharacter = value; }
-        }
-
         private static String gs_GameVersion = "Alpha 0.0.2.0";
 
         public static String GameVersion
