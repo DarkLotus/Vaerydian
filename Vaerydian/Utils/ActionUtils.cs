b35c79c2d69cb9fbc8753c8d3544444998b6de21
NetGnome <thjonell@gmail.com>
2013-03-11 04:57:15 -0400

    renaming ability utils to action utils clean compile

--- Vaerydian/Utils/ActionUtils.cs
+++ Vaerydian/Utils/ActionUtils.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace Vaerydian
 {
-	public static class AbilityUtils
+	public static class ActionUtils
 	{
 
 		public static float getProbability(int skillValue, int attributeValue, float knowledgeValue, int speedValue){
-			return 
+			return 0f;
 		}
 
 
