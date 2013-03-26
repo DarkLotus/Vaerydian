daa70a57d178845c4c2786e625c8cbd6ca688c7a
NetGnome <thjonell@gmail.com>
2013-03-25 20:05:14 -0400

    added inital actions.v config file

---
+++ Content/json/actions.v
@@ -0,0 +1,52 @@
+{
+  "action_types": {
+    "DAMAGE" : 0,
+    "MODIFY" : 1,
+    "CREATE" : 2,
+    "DESTROY" : 3,
+  },
+  "impacts_type" : {
+    "DIRECT" : 0,
+    "AREA" : 1,
+    "OVERTIME" : 2,
+  },
+  "damage_types" : {
+    "PHYSICAL": 0,
+    "ICE": 1,
+    "FIRE": 2,
+    "EARTH": 3,
+    "WIND": 4,
+    "WATER": 5,
+    "LIGHT": 6,
+    "DARK": 7,
+    "CHAOS": 8,
+    "ORDER": 9,
+    "POISON": 10,
+    "DISEASE": 11,
+    "ARCANE": 12
+  },
+  "modify_types" : {
+    "SKILL" : 0,
+    "ABILITY" : 1,
+    "MECHANIC" : 2,
+    "ATTRIBUTE" : 3,
+    "KNOWLEDGE" : 4
+  },
+  "modify_duration" : {
+    "TEMPORARY" : 0,
+    "PERMANENT" : 1,
+    "LOCATION" : 2,
+  }
+  "create_types" : {
+    "OBJECT" : 0,
+    "CHARACTER" : 1,
+    "ITEM" : 2,
+    "FEATURE" : 3
+  },
+  "destroy_types" : {
+    "OBJECT" : 0,
+    "CHARACTER" : 1,
+    "ITEM" : 2,
+    "FEATURE" : 3
+  }
+}
